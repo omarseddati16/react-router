9150d70e790212ea12b64b90b023c9ca125f3aec
omarseddati16 <omarseddati16@gmail.com>
2025-09-08 15:41:18 +0200

    [UPD] bonus, add button with useNavigate

--- src/pages/ProductsDetail.jsx
+++ src/pages/ProductsDetail.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios'
 import { useState, useEffect } from "react"
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
+
 
 function ProductsDetail() {
 
   const { id } = useParams()
   const [product, setProduct] = useState({});
+  const navigate = useNavigate();
+
 
   useEffect(() => {
     axios.get(`https://fakestoreapi.com/products/${id}`).then((resp) =>
@@ -19,6 +22,16 @@ function ProductsDetail() {
       <p className='mt-4'>{product.description}</p>
       <p>€ {product.price}</p>
       <p>Categoria: {product.category}</p>
+
+      <div className="d-flex justify-content-between mt-4">
+        <button onClick={() => navigate(`/product/${product.id - 1}`)}>
+          Prodotto Precedente
+        </button>
+
+        <button onClick={() => navigate(`/product/${product.id + 1}`)}>
+          Prodotto Successivo
+        </button>
+      </div>
     </div>
   )
 }
